670a096c98c92b5abe1baba94c8f1edea2897309
Arondevera02 <josephjumawan02@gmail.com>
2024-10-24 04:51:16 +0800

    negga

--- naturemedix/lib/main.dart
+++ naturemedix/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:naturemedix/admin_profile.dart';
 import 'package:naturemedix/remedy_info.dart';
 import 'package:naturemedix/request.dart';
 import 'package:naturemedix/request_info.dart';
+import 'package:naturemedix/user_profile.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,7 +24,8 @@ class MyApp extends StatelessWidget {
       // home: const RequestInfo(id: '', userId: '', plantName: '', scientificName: '', description: '', imageUrl: '', handleBy: '', status: '', lastUpdated: '', dateCreated: '',),
       // home: const RequestPlantForm(),
       // home: const RemedyInfo(),
-      home: const AdminProfile(),
+      // home: const AdminProfile(),
+      home: const UserProfile(),
     );
   }
 }
